143068a38071de9424d41f07f6c1a7aacf43d067
Your Name <you@example.com>
2024-05-13 20:36:22 +0700

    U[p

--- config/config.inc.php
+++ config/config.inc.php
@@ -3,9 +3,6 @@ session_start();
 define("DB_TYPE", "MySQL"); // MySQL & SQLite
 define("DB_HOST", "localhost");
 
-// define("DB_USERNAME", "signsoft_signsoft");
-// define("DB_PASSWORD", "signsoft_signsoft");
-// define("DB_NAME", "signsoft_paper");
 
 
 define("DB_USERNAME", "root");
